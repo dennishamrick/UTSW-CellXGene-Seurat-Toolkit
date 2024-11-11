ba21e9c5df45b341ec14a77fd121ae034c5e9c26
Dennis Hamrick <149102712+dennishamrick@users.noreply.github.com>
2024-11-11 12:10:33 -0600

    Update editing_gene_names.py

--- editing_gene_names.py
+++ editing_gene_names.py
@@ -1,11 +1,17 @@
+### CellxGene stores gene names as ENSMBL ID's, which are useful for standardization but are not easy to read for humans.
+
+## Use this script to change ENSEMBL ID's to standard gene names. Very useful if you plan to analyze in Seurat. Edit the name of the h5ad as needed.
+
+##
+
 import pandas as pd
 import numpy as np
 import scanpy as sc
 import anndata as ad
 
-b2=sc.read_h5ad("b2.h5ad")
-gene_name = b2.var_vector('gene_name')
-b2.var_names = b2.var["gene_name"].copy()
-b2.var.index.name = None 
-b2.obs.index.name = None 
-b2.write_h5ad("brain2_edited_genenames.h5ad")
+update_names=sc.read_h5ad("YOUR_CELLXGENE_FILE.h5ad")
+gene_name = update_names.var_vector('gene_name')
+update_names.var_names = update_names.var["gene_name"].copy()
+update_names.var.index.name = None 
+update_names.obs.index.name = None 
+update_names.write_h5ad("YOUR_CELLXGENE_FILE_updated_genenames.h5ad")
