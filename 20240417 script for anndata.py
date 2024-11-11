3054e69a25e4b4bef427ba7aa0515e7d91e3ba3d
Dennis Hamrick <149102712+dennishamrick@users.noreply.github.com>
2024-11-11 12:04:50 -0600

    python_script_to_add_annotations_to_cellxgene_file.py

--- 20240417 script for anndata.py
+++ 20240417 script for anndata.py
@@ -1,24 +1,15 @@
+##Run this in python, edit names/location of file as needed for use before running
+
 import pandas as pd
 import scanpy as sc
 
-new_annotations = pd.read_csv('brain1_coronal-cell-labels-R7J3MXWU.csv',
+new_annotations = pd.read_csv('YOUR_ANNOTATIONS_FILE.csv',
                          comment='#',
                          dtype='category',
                          index_col=0)
-new1 = sc.read('brain1-coronal.h5ad')
+new1 = sc.read('NAME_OF_ORIGINAL_CELLXGENE_FILE.h5ad')
 new1.obs = new1.obs.join(new_annotations)
 
-new1.write('brain1-coronal-annotated.h5ad')
-
-
-new_annotations = pd.read_csv('abcd-cell-labels-X3IQP5VJ.csv',
-                         comment='#',
-                         dtype='category',
-                         index_col=0)
-new2 = sc.read('animal2-coronal.h5ad')
-new2.obs = new2.obs.join(new_annotations)
-
-
-new2.write('animal2-coronal-annotated.h5ad')
+new1.write('NAME_OF_ORIGINAL_CELLXGENE_FILE_annotated.h5ad')
 
 
