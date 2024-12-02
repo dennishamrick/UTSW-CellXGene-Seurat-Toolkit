a0c18142e87462031d54443544d980f9de92221f
dennis <dennis@localhost.localdomain>
2024-12-02 14:23:40 -0600

    edited edit_gene_names.py

--- edit_gene_names.py
+++ edit_gene_names.py
@@ -3,24 +3,34 @@
 ## Use this script to change ENSEMBL ID's to standard gene names. Very useful if you plan to analyze in Seurat. Edit the name of the h5ad as needed.
 
 ##
+print("Importing packages...")
 import os
 import sys
 import pandas as pd
 import numpy as np
 import scanpy as sc
 import anndata as ad
+from pathlib import Path
+print('Packages imported successfully.')
+cwd = os.getcwd()
+print('Current working directory is ' + cwd + '. Output file will be written here.') 
 
-filename = input("Enter your .h5ad filename:")
+while True:
+    filename = input("Enter your .h5ad path: ")
+    if not os.path.exists(filename):
+        print("h5ad file not found.")
+    else:
+        break
 
-if not os.path.exists(filename):
-    sys.exit("File not found.")
-print("Reading h5ad...")
-update_names=sc.read_h5ad(filename)
-print("h5ad read. Updating gene names...")
+basename = Path(filename).stem
+print("Reading .h5ad... ")
+new1 = sc.read(filename)
+print(".h5ad read successfully.")
+print("Updating gene names...")
 gene_name = update_names.var_vector('gene_name')
 update_names.var_names = update_names.var["gene_name"].copy()
 update_names.var.index.name = None 
 update_names.obs.index.name = None 
-printf("Done! Writing h5ad...")
-update_names.write_h5ad(filename + "_updated_genenames.h5ad")
-printf("h5ad written to " filename + "_updated_genenames.h5ad.")
+print("Done! Writing h5ad...")
+update_names.write_h5ad(basename + "_updated_genenames.h5ad")
+print("h5ad written to " + basename + "_updated_genenames.h5ad.")
