4a6ab03c9d2c4df7cc83da256ce4e6410209ed8e
dennis <dennis@localhost.localdomain>
2024-12-02 14:08:00 -0600

    user-friendly-ized the script for anndata

--- 20240417 script for anndata.py
+++ 20240417 script for anndata.py
@@ -1,15 +1,52 @@
 ##Run this in python, edit names/location of file as needed for use before running
-
+print('Importing packages')
+import os
+import sys
 import pandas as pd
 import scanpy as sc
+import anndata as ad
+from pathlib import Path
+print('Packages imported successfully.')
+cwd = os.getcwd()
+print('Current working directory is ' + cwd + '. Output file will be written here.') 
 
-new_annotations = pd.read_csv('YOUR_ANNOTATIONS_FILE.csv',
+while True:
+    filename = input("Enter your .h5ad path: ")
+    if not os.path.exists(filename):
+        print("h5ad file not found.")
+    else:
+        break
+basename = Path(filename).stem
+print("Reading .h5ad... ")
+new1 = sc.read(filename)
+print(".h5ad read successfully.") 
+
+while True:
+    annotations = input("Enter your .csv annotations path: ")
+    if not os.path.exists(annotations):
+        print("Annotations file not found.")
+    else: 
+        break
+print("Reading .csv...")
+new_annotations = pd.read_csv(annotations,
                          comment='#',
                          dtype='category',
                          index_col=0)
-new1 = sc.read('NAME_OF_ORIGINAL_CELLXGENE_FILE.h5ad')
+print(".csv read successfully. Joining...")
 new1.obs = new1.obs.join(new_annotations)
-
-new1.write('NAME_OF_ORIGINAL_CELLXGENE_FILE_annotated.h5ad')
+while True:
+    answer = input("New .h5ad created! Compressed files (gzip format) will take up much less space but will be slower to load. Would you like to compress the output file? Enter y or n: ")
+    if answer.lower() == 'y':
+        print('Writing compressed .h5ad...')
+        new1.write_h5ad(basename + '_annotated.h5ad', compression = 'gzip')
+        print('New h5ad written to ' + cwd)
+        exit()
+    elif answer.lower() == 'n':
+        print('Writing .h5ad...')
+        new1.write_h5ad(basename + '_annotated.h5ad')
+        print('New h5ad written to ' + cwd)
+        exit()
+    else: 
+        print("Invalid input. Please enter y or n.")
 
 
--- editing_gene_names.py
+++ editing_gene_names.py
@@ -3,15 +3,24 @@
 ## Use this script to change ENSEMBL ID's to standard gene names. Very useful if you plan to analyze in Seurat. Edit the name of the h5ad as needed.
 
 ##
-
+import os
+import sys
 import pandas as pd
 import numpy as np
 import scanpy as sc
 import anndata as ad
 
-update_names=sc.read_h5ad("YOUR_CELLXGENE_FILE.h5ad")
+filename = input("Enter your .h5ad filename:")
+
+if not os.path.exists(filename):
+    sys.exit("File not found.")
+print("Reading h5ad...")
+update_names=sc.read_h5ad(filename)
+print("h5ad read. Updating gene names...")
 gene_name = update_names.var_vector('gene_name')
 update_names.var_names = update_names.var["gene_name"].copy()
 update_names.var.index.name = None 
 update_names.obs.index.name = None 
-update_names.write_h5ad("YOUR_CELLXGENE_FILE_updated_genenames.h5ad")
+printf("Done! Writing h5ad...")
+update_names.write_h5ad(filename + "_updated_genenames.h5ad")
+printf("h5ad written to " filename + "_updated_genenames.h5ad.")
